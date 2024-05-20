daa0f0bce11c4d4cadc63dc7f11149a385cd75df
sajan rabha <sajanrabh971@gmail.com>
2024-05-20 15:23:42 +0530

    add debug1.cpp to new codebase

---
+++ Week1Debug/debug1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+
+//Take input a character, print 1, if its a capital alphabet, 
+// print 0, if its a lowercase alphabet, else print -1.
+int main() {
+	char c;
+    cin>>c;
+    if('a'<=c && c<= 'z'){
+        cout<<0;
+    }
+	else if('A'<=c && c<= 'Z'){
+        cout<<1;
+    }
+    else{
+        cout<<-1;
+    }
+}
