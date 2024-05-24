475ff6c732a3d756da86ec8b42a640c7b6a8ff39
sajan rabha <sajanrabh971@gmail.com>
2024-05-25 00:38:33 +0530

    add file to the codebase

--- exp.cpp
+++ exp.cpp
@@ -1,21 +1,7 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int length;
-    int width;
-    cout<<"enter length";
-    cin>>length;
-    cout<<"enter width";
-    cin>>width;
-    for(int row = 0; row < length; row++){
-        for(int col = 0; col < width; col++){
-            if(row == 0 || row == length-1 || col == 0 || col == width-1)
-            cout<<"* ";
-            else
-            cout<<"  ";
-        }
-        cout<<endl;
-    }
 
+    cout<< ~5;
     return 0;
 }
